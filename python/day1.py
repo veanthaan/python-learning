5fa28feec8822c34e1fd7cd03bd2838effc2a343
veanthaan <veanthaan22@gmail.com>
2026-04-21 17:03:59 +0530

    Python usecases
    day 01-03

---
+++ python/day1.py
@@ -0,0 +1,20 @@
+name = "hp"
+age = 23
+print(name)
+print(age)
+
+name = input("enter your name:")
+print("hello",name)
+
+#addition
+
+a = 10
+b = 5
+print("sum is:",a+b)
+
+a = int(input("enter your number 1:"))
+b = int(input("Enter your number 2:"))
+print("total:",a+b)
+
+age = int(input("Enter your age:hp"))
+print(f"you are {age} years old")
